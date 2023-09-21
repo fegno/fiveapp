7387e703c089e804a368e52a2e1430c8b374ee06
Sherin Fathima <sherinfathima925@gmail.com>
2023-09-21 11:35:34 +0530

    payment section

--- payment/urls.py
+++ payment/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, re_path
 
 urlpatterns = [
     re_path(r'^initiate-payment/', api.InitiatePayment.as_view(), name='initiate-payment'),
+    re_path(r'^payment-webhook/', api.StripePaymentWebhook.as_view(), name='payment-webhook'),
+
 ]
