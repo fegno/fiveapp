9109c9baa4101727678df7077f187e97e3361bda
shilpasasidharan <shilpa.fegno@gmail.com>
2023-09-21 15:25:49 +0530

    sendgrid

--- fiveapp/settings.py
+++ fiveapp/settings.py
@@ -133,12 +133,21 @@ STATIC_ROOT = os.environ.get("STATIC_ROOT")
 MEDIA_ROOT = os.environ.get("MEDIA_ROOT")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD =  os.environ.get("EMAIL_HOST_PASSWORD")
+
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD =  os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'apikey'  # This is the username for SendGrid SMTP
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'shilpa.fegno@gmail.com')
 
 DISABLE_INTERNAL_REQUEST_CALL_API = False
 STRIPE_API_KEY="sk_test_51NpuyVSCbaUjXphJu1nqqETnMprV6N2qi0F2kqAkJh2zE0xX22Ai93g2eeDEAkXAbG6VEKTxAEyL7BUeuqwbMIky00BL8tHNaQ"
