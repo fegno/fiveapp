554a77a91c2925a24590175245dfbc3efc9231b0
shilpasasidharan <shilpa.fegno@gmail.com>
2023-09-26 18:39:58 +0530

    condition check

--- administrator/api.py
+++ administrator/api.py
@@ -624,18 +624,22 @@ class AssignUser(APIView):
             except UserProfile.DoesNotExist:
                 response_dict["error"] = f"User with the ID {user_id} does not exsts"
 
-            if UserAssignedModules.objects.filter(user=user_id).exists():
-                if UserAssignedModules.objects.filter(user=user_id, module=module).exists():
-                    response_dict["message"] = f"User {user_profile.id} is already assigned to module {module.id}"
-                else:
-                    user_assign_object = UserAssignedModules.objects.filter(user=user_id).first()
-                    user_assign_object.module.add(module)
-                    response_dict["message"] = "User is assigned to the module"
-                    continue
+            subscribed_module = SubscriptionDetails.objects.filter(user=request.user, module=pk)
+            if subscribed_module:
+                if UserAssignedModules.objects.filter(user=user_id).exists():
+                    if UserAssignedModules.objects.filter(user=user_id, module=module).exists():
+                        response_dict["message"] = f"User {user_profile.id} is already assigned to module {module.id}"
+                    else:
+                        user_assign_object = UserAssignedModules.objects.filter(user=user_id).first()
+                        user_assign_object.module.add(module)
+                        response_dict["message"] = "User is assigned to the module"
+                        continue
 
-            assign_user = UserAssignedModules.objects.create(user=user)
-            assign_user.module.add(module)
-            response_dict["message"] = f"User with ID {user_profile.id} added to the module id{module.id}"
+                assign_user = UserAssignedModules.objects.create(user=user)
+                assign_user.module.add(module)
+                response_dict["message"] = f"User with ID {user_profile.id} added to the module id{module.id}"
+            else:
+                response_dict["error"] = "Not subscribed the module"
         return Response(response_dict, status=status.HTTP_200_OK)
     
     
