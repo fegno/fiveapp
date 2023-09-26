dcf178e8e3da67e8f4bbe6673c3a192be6128884
Sherin Fathima <sherinfathima925@gmail.com>
2023-09-26 13:56:42 +0530

    issue fix and new api

--- administrator/urls.py
+++ administrator/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     re_path(r'^home-page/', api.Homepage.as_view(), name='home-page'),
     re_path(r'^list-subscription-plans/', api.ListSubscriptionPlans.as_view(), name='list-subscription-plans'),
     re_path(r'^view-bundle-details/(?P<pk>\d+)/$', api.ViewBundle.as_view()),
-    re_path(r'^list-modules/', api.ListModules.as_view(), name='list-modules'),
     re_path(r'^select-free-subscription/', api.SelectFreeSubscription.as_view(), name='select-free-subscription'),
 
+    re_path(r'^list-bundle-modules/(?P<pk>\d+)/', api.ListBundleModules.as_view(), name='list-bundle-modules'),
+    re_path(r'^list-modules/', api.ListModules.as_view(), name='list-modules'),
     re_path(r'^module-users/(?P<pk>\d+)/$', api.UserInModule.as_view(), name='users-under-the-module'),
     re_path(r'^user-delete/(?P<pk>\d+)/(?P<module_id>\d+)/$', api.DeleteUserFromModule.as_view(), name='delete-user'),
     re_path(r'^user-un-assign/(?P<pk>\d+)/$', api.UnAssignUserlist.as_view(), name='user-unassign'),
