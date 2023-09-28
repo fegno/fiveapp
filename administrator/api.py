7edb64a8403b45a9e65ed4bf3d901e2592ae0227
Sherin Fathima <sherinfathima925@gmail.com>
2023-09-28 11:40:00 +0530

    status added in report

--- administrator/api.py
+++ administrator/api.py
@@ -664,6 +664,12 @@ class AnalyticsReport(APIView):
             When(team_actual_working_hr=F("team_working_hr"), then=Value("Standard")),
 
         ]
+        absent_status_list = [
+            When(team_absent_days__gte=3, then=Value("Overloaded")),
+            When(team_absent_days__gt=1,team_absent_days__lt=3 , then=Value("Underloaded")),
+            When(team_absent_days__lte=1, then=Value("Standard")),
+
+        ]
 
         if csv_file.modules.title != "Team Indicator":
             log  = CsvLogDetails.objects.filter(
@@ -682,13 +688,17 @@ class AnalyticsReport(APIView):
                 status=Case(
                     *status_list, default=Value(""), output_field=CharField()
                 ),
+                absent_status=Case(
+                    *absent_status_list, default=Value(""), output_field=CharField()
+                ),
             ).values(
                 "team", 
                 "employee_count",
                 "team_working_hr", "team_absent_days",
                 "avg_team_working_hr",
                 "team_actual_working_hr",
-                "status"
+                "status",
+                "absent_status"
             )
 
             total_absent_days = log.aggregate(total=Sum("team_absent_days"))
@@ -703,7 +713,7 @@ class AnalyticsReport(APIView):
             total_employee = log.aggregate(total=Sum("employee_count"))
             response_dict["total_employee"] = total_employee.get("total") if total_employee else 0
             response_dict["working_hours_report"] = log.values("team", "status", "employee_count", "team_working_hr", "team_actual_working_hr")
-            response_dict["absent_days_report"] = log.values("team","status", "employee_count", "team_absent_days")
+            response_dict["absent_days_report"] = log.values("team","absent_status", "employee_count", "team_absent_days")
 
         
         response_dict["status"] = True
